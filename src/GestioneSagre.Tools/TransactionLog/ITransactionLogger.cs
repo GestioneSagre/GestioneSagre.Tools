88bb924584bc8631f0644927f2098cf5e00cfc54
Angelo Pirola <angelo9871@gmail.com>
2022-07-13 15:00:18 +0200

    Aggiunti nuovi metodi

---
+++ src/GestioneSagre.Tools/TransactionLog/ITransactionLogger.cs
@@ -0,0 +1,13 @@
+﻿using GestioneSagre.Models.InputModels.Feste;
+using GestioneSagre.Models.InputModels.Logo;
+using GestioneSagre.Models.InputModels.Versioni;
+
+namespace GestioneSagre.Tools.TransactionLog;
+
+public interface ITransactionLogger
+{
+    Task LogTransactionVersioneCreateAsync(VersioneCreateInputModel inputModel);
+    Task LogTransactionLogoEditAsync(LogoEditInputModel inputModel);
+    Task LogTransactionFestaCreateAsync(FestaCreateInputModel inputModel);
+    Task LogTransactionFestaEditAsync(FestaEditInputModel inputModel);
+}
---
+++ src/GestioneSagre.Tools/TransactionLog/LocalTransactionLogger.cs
@@ -0,0 +1,97 @@
+﻿using GestioneSagre.Models.InputModels.Feste;
+using GestioneSagre.Models.InputModels.Logo;
+using GestioneSagre.Models.InputModels.Versioni;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GestioneSagre.Tools.TransactionLog;
+
+public class LocalTransactionLogger : ITransactionLogger
+{
+    private readonly IHostingEnvironment env;
+    private readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+    public LocalTransactionLogger(IHostingEnvironment env)
+    {
+        this.env = env;
+    }
+
+    public async Task LogTransactionFestaCreateAsync(FestaCreateInputModel inputModel)
+    {
+        string filePath = Path.Combine(env.ContentRootPath, "Data", "transactions.txt");
+        string content = $"\r\n{inputModel.DataInizio}\t{inputModel.DataFine}\t{inputModel.Titolo}\t{inputModel.Edizione}\t{inputModel.Luogo}";
+
+        CheckLogsFolderExist(filePath);
+
+        try
+        {
+            await semaphore.WaitAsync();
+            await File.AppendAllTextAsync(filePath, content);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    public async Task LogTransactionFestaEditAsync(FestaEditInputModel inputModel)
+    {
+        string filePath = Path.Combine(env.ContentRootPath, "Data", "transactions.txt");
+        string content = $"\r\n{inputModel.DataInizio}\t{inputModel.DataFine}\t{inputModel.GuidFesta}\t{inputModel.Titolo}\t{inputModel.Edizione}\t{inputModel.Luogo}";
+
+        CheckLogsFolderExist(filePath);
+
+        try
+        {
+            await semaphore.WaitAsync();
+            await File.AppendAllTextAsync(filePath, content);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    public async Task LogTransactionLogoEditAsync(LogoEditInputModel inputModel)
+    {
+        string filePath = Path.Combine(env.ContentRootPath, "Data", "transactions.txt");
+        string content = $"\r\n{inputModel.GuidFesta}\t{inputModel.Logo.Name}";
+
+        CheckLogsFolderExist(filePath);
+
+        try
+        {
+            await semaphore.WaitAsync();
+            await File.AppendAllTextAsync(filePath, content);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    public async Task LogTransactionVersioneCreateAsync(VersioneCreateInputModel inputModel)
+    {
+        string filePath = Path.Combine(env.ContentRootPath, "Data", "transactions.txt");
+        string content = $"\r\n{inputModel.TestoVersione}";
+
+        CheckLogsFolderExist(filePath);
+
+        try
+        {
+            await semaphore.WaitAsync();
+            await File.AppendAllTextAsync(filePath, content);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    private static void CheckLogsFolderExist(string filePath)
+    {
+        if (!Directory.Exists(filePath))
+        {
+            Directory.CreateDirectory(filePath);
+        }
+    }
+}
